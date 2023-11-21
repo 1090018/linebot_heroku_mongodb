347dff75d5ca1c0f161f5e6a2ac84c74d31c7caa
租圈 <135095541+1090018@users.noreply.github.com>
2023-11-21 14:33:47 +0800

    Update noitce.py

--- noitce.py
+++ noitce.py
@@ -8,17 +8,11 @@ import datetime
 
 app = Flask(__name__)
 
-# 設置 LINE Bot 的 Channel Access Token 和 Channel Secret
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
 # LINE Bot 的訊息處理
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message_text = event.message.text.lower()
-    if '繳錢' in message_text:  # 當收到包含「繳錢」的訊息時
-        reply_text = "別忘了每個月的5日要繳錢喔！"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+def handle_message():
+    reply_text = "別忘了每個月的5日要繳錢喔！"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 # 設置每月5日的提醒
 def remind_to_pay():
