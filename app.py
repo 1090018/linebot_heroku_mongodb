41043a8d3a2ad14a752dcfb95852ae4b3dbbab0a
租圈 <135095541+1090018@users.noreply.github.com>
2023-12-05 20:09:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,12 @@ def handle_message(event):
                 alt_text = '繳款通知',
                 contents = json.load(open('unpaid_notice.json', 'r', encoding='utf-8'))  
             )) 
+
+    elif '繳費查詢' in msg:
+        line_bot_api.reply_message(event.reply_token,
+            FlexSendMessage(
+                alt_text = '繳費查詢',
+                contents = json.load(open('paid_read.json', 'r', encoding='utf-8'))  
         
     elif '合約專區' in msg:
         message = buttons_message_contract()
