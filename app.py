156d515f65f9b03a4542589d190e5efdbdea8eb2
租圈 <135095541+1090018@users.noreply.github.com>
2023-12-14 01:13:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def handle_message(event):
                 alt_text = '簽約注意事項',
                 contents = json.load(open('sign.json', 'r', encoding='utf-8'))  
             )) 
-    elif msg =='租房須知':
+    elif msg =='租屋須知':
         message = test()
         line_bot_api.reply_message(event.reply_token, message)
         
