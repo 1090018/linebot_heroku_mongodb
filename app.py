36f8c6bd193ae0db21f4c82655aa679863d209a6
租圈 <135095541+1090018@users.noreply.github.com>
2023-12-18 23:50:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def handle_message(event):
     #==========資料庫(查詢對話紀錄功能)=============
     #============確認功能==============
         message2 = Confirm_Template()
-        line_bot_api.push_message(userid(), message2)  
+        userid = userid()
+        line_bot_api.push_message(userid, message2)  
     #============確認功能============== 
     
     elif msg =='簽約流程一覽':
@@ -152,7 +153,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)   
     #=======資料庫(刪除功能)===========
     #==========房屋篩選===============
-        line_bot_api.push_message(userid(),
+        userid = userid()
+        line_bot_api.push_message(userid,
             FlexSendMessage(
                 alt_text = '房屋條件篩選',
                 contents = json.load(open('select.json', 'r', encoding='utf-8'))  
